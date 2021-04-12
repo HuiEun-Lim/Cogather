4adae70f1f584b61ed13cdb045f863a906a3ccee
5STARJeongHee <qhxmaoflr@gmail.com>
2021-04-12 21:14:11 +0900

    댓글, 파일 업로드, 삭제 주석 작업

--- src/main/java/com/project/cogather/content/service/BoardService.java
+++ src/main/java/com/project/cogather/content/service/BoardService.java
@@ -29,45 +29,47 @@ public class BoardService {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	// 게시글 목록 보이기
 	public List<ContentDTO> list(int from, int pageRows, int sg_id){
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		return contentDAO.selectFromRow(from, pageRows, sg_id);
 	}
-	
+	// 게시글 수 세기
 	public int countAll(int sg_id) {
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		return contentDAO.countAll(sg_id);
 	}
-	
+	// 게시글 볼 때 조회수 증가 및 하나의 게시글 읽어오기
 	@Transactional
 	public List<ContentDTO> viewByUid(int sg_id, int ct_uid){
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		contentDAO.incViewCnt(sg_id, ct_uid);
 		return contentDAO.selectByUid(sg_id, ct_uid);
 	}
+	// 이미지 업로드를 위해 임시 게시글 생성후 임시 게시글 반환 - 이게 글 작성위한 부분이 됨
 	@Transactional
 	public List<ContentDTO> insert(ContentDTO dto) {
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		contentDAO.insert(dto);
 		return contentDAO.selectTempContent(dto.getId());
 	}
-	
+	// 이부분이 글 수정을 위하기도 하고 글을 작성하는 부분이기도 함
 	public int update(ContentDTO dto) {
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		return contentDAO.update(dto);
 	}
-
+	// 글 삭제
 	public int deleteByUid(int sg_id, int ct_uid, String id) {
 		contentDAO = sqlSession.getMapper(ContentDAO.class);
 		
 		return contentDAO.deleteByUid(sg_id, ct_uid, id);
 	}
-	
+	// 업로드된 파일 물리적인 삭제
 	public int deleteFiles(int ct_uid , HttpServletRequest request) {
 		contentFileDAO = sqlSession.getMapper(ContentFileDAO.class);
 		List<ContentFileDTO> list = contentFileDAO.getFileInfosByCTUID(ct_uid);
 		int cnt = 0;
-		String url = request.getSession().getServletContext().getRealPath(Common.CONTENTFILEPATH);
+		String url = request.getSession().getServletContext().getRealPath(Common.CONTENTFILEPATH); // 파일들이 저장된 경로를 가져옴
 		
 		if(list == null) {
 			return 0;
@@ -75,8 +77,8 @@ public class BoardService {
 		
 		for (int i = 0; i< list.size(); i++) {
 			ContentFileDTO dto = list.get(i);
-			File file = new File(url+"\\"+dto.getCf_file());
-			if(file.exists()) {
+			File file = new File(url+"\\"+dto.getCf_file()); // 경로와 합쳐서 실제 파일에 대한 객체를 얻어옴
+			if(file.exists()) { // 파일이 존재하면 삭제하고 아니면 넘어감
 				if(file.delete()) {
 					System.out.println(url+"\\"+dto.getCf_file()+"물리적 파일 삭제 성공");
 					cnt += 1;
@@ -89,17 +91,17 @@ public class BoardService {
 		}
 		return cnt;
 	}
-	
+	// 에디터로부터 저장 요청이 들어오면 해당 파일을 저장
 	public ContentFileDTO saveBoardFile(int ct_uid, MultipartHttpServletRequest mpRequest) {
 		contentFileDAO = sqlSession.getMapper(ContentFileDAO.class);
 		
 		String fileName =null;
 		ContentFileDTO dto = null;
 		Iterator<String> filenameitr = mpRequest.getFileNames();
-		String url = mpRequest.getSession().getServletContext().getRealPath(Common.CONTENTFILEPATH);
-		File saveFolder = new File(url);
+		String url = mpRequest.getSession().getServletContext().getRealPath(Common.CONTENTFILEPATH); // 저장할 폴더 경로 실제 경로
+		File saveFolder = new File(url); // 저장 경로로 파일 객체 생성
 		
-		if(!saveFolder.exists()) {
+		if(!saveFolder.exists()) { // 파일이 없다- 즉 경로가 없다면 생성
 			try {
 				saveFolder.mkdirs();
 				System.out.println("저장경로의 폴더가 없었으므로 생성합니다.");
@@ -110,19 +112,19 @@ public class BoardService {
 		
 		try {
 			while(filenameitr.hasNext()) {
-				fileName = filenameitr.next();
-				MultipartFile file = mpRequest.getFile(fileName);
-				String orgFileName = file.getOriginalFilename();
-				dto = new ContentFileDTO();
+				fileName = filenameitr.next(); // 파일 이름 가져옴 -- ckeditor에서 upload라는 이름으로 줌
+				MultipartFile file = mpRequest.getFile(fileName); // 파일 객체를 얻음
+				String orgFileName = file.getOriginalFilename(); // 파일 객체로 실제 원본 이름 가져옴
+				dto = new ContentFileDTO(); 
 				dto.setCf_source(orgFileName);
 				
 				dto.setCt_uid(ct_uid);
 				String orgFileExtension = orgFileName.substring(orgFileName.lastIndexOf("."));
-				String storedFileName = UUID.randomUUID().toString().replaceAll("-", "") + orgFileExtension;
+				String storedFileName = UUID.randomUUID().toString().replaceAll("-", "") + orgFileExtension; // 저장할 이름은 랜덤하게 해준다.
 				
 				dto.setCf_file(storedFileName);
-				contentFileDAO.saveFile(dto);
-				file.transferTo(new File(url+"\\"+storedFileName));
+				contentFileDAO.saveFile(dto); // db에 해당 정보를 저장
+				file.transferTo(new File(url+"\\"+storedFileName)); // 파일 객체로부터 실제 저장될 곳으로 파일을 복사해서 옮김
 			}
 			
 		} catch (IllegalStateException | IOException e) {
--- src/main/webapp/JS/studyroomBoard.js
+++ src/main/webapp/JS/studyroomBoard.js
@@ -28,38 +28,38 @@ $(function(){
 		loadBoard(window.page);  // 현재 페이지 목록 다시 로딩
 		togglePage("list-mode");// 본래 리스트 화면으로
 	});
-	$("#write-send").click(function (event){
+	$("#write-send").click(function (event){ // 글작성이지만 임시 글에대한 수정으로 들어가도록 변경
 		event.preventDefault();
 		chkUpdate();
 	})
-	$("#view-delete").click(function(event){
+	$("#view-delete").click(function(event){ // 글 삭제 버튼 클릭
 		event.preventDefault();
 		chkDelete(); // 삭제
 	});
 	
-	$("#view-update").click(function(event){
+	$("#view-update").click(function(event){ // 글 수정 버튼 클릭시 이벤트
 		event.preventDefault();
 		togglePage("update-mode");
 	});
-	$("#update-cancel").click(function(){
+	$("#update-cancel").click(function(){ // 글 수정 중 글을 수정하지 않고 되돌아 갈때
 		togglePage("view-mode");
 	});
-	$("#update-send").click(function(event){
+	$("#update-send").click(function(event){// 글 수정 제출 이벤트
 		event.preventDefault();
 		chkUpdate();
 	});
-	$("#comment-register").click(function(event){
+	$("#comment-register").click(function(event){ // 댓글 작성 이벤트
 		event.preventDefault();
 		sendComment();
 	});
-	$(".comment-box i").click(function(event){
+	$(".comment-box i").click(function(event){ // 댓글 새로고침 버튼 클릭 이벤트
 		event.preventDefault();
-		loadComments(viewItem.data[0].ct_uid);
+		loadComments(viewItem.data[0].ct_uid); // 게시글 아이디로 새로 받아서 뿌려줌
 	});
 	
 });
 
-function loadBoard(page){
+function loadBoard(page){ // 방 번호와 페이지, 페이지에 표시할 글의 수로 데이터를 받아옴
 	togglePage("list-mode");
 	$.ajax({
         url : contextPath+"/group/studyboard/"+roomId+"/page/"+page+"/"+pageRows,
@@ -77,8 +77,8 @@ function loadBoard(page){
 }
         }
     });  // end $.ajax()
-}
-
+}// end loadBoard(page)
+// 방 번호와 페이지, 페이지에 표시할 글의 수로 데이터를 받아온 걸 토대로 글 리스트를 뿌려줌
 function updateList(jsonObj){
 	var result = "";
 	
@@ -125,6 +125,7 @@ function updateList(jsonObj){
 	}
 } // updateList() end
 
+// 페이징 정보를 뿌려줌
 function buildPagination(writePages, totalPage, curPage){
 	var str = "";   // 최종적으로 페이징에 나타날 HTML 문자열 <li> 태그로 구성
 	
@@ -168,89 +169,89 @@ function buildPagination(writePages, totalPage, curPage){
     return str;
 } // end buildPagination();
 
-//<select> 에서 pageRows 값 변경할때마다
+//<select> 에서 pageRows 값 변경할때마다 새로 글 목록 업데이트
 function changePageRows(){
     window.pageRows = $("#rows").val();
     loadBoard(window.page);
-}
+} // end changePageRows()
 
+// 특정 버튼 클릭시 원하는 화면을 구성하기 위해 특정요소를 숨기고 보여줄 요소들을 드러나게 하는 함수
 function togglePage(mode){
 	if (mode == "write-mode"){
-		$(".board-list").hide();
-		$("#view-mode").hide();
-		$("#write-mode").show();
+		$(".board-list").hide(); // 게시글 가리기
+		$("#view-mode").hide(); // view 가리기
+		$("#write-mode").show(); // 입력 화면 보이기
 		
-		$(".article-header .writer-info").html('');
-		$(".article-container").html('');
+		$(".article-header .writer-info").html(''); // view의 일부 정보 초기화
+		$(".article-container").html(''); // // view의 일부 정보 초기화
 		
-		$(".btn_group_write #write-list").attr("disabled",false);
-		$(".btn_group_write #write-send").attr("disabled",false);
-		$(".btn_group_view #view-list").attr("disabled", true);
-		$(".btn_group_view #view-update").attr("disabled", true);
-		$(".btn_group_view #view-delete").attr("disabled", true);
-		$(".btn_group_view #view-write").attr("disabled", true);
-		$(".btn_group_update #update-cancel").attr("disabled",true);
-		$(".btn_group_update #update-send").attr("disabled",true);
-		$(".btn_group_update #update-cancel").hide();
-		$(".btn_group_update #update-send").hide();
-		$(".comment-list").html('');
-		createTempContent();
+		$(".btn_group_write #write-list").attr("disabled",false); // write 시 필요한 버튼활성화
+		$(".btn_group_write #write-send").attr("disabled",false); // write 시 필요한 버튼활성화
+		$(".btn_group_view #view-list").attr("disabled", true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_view #view-update").attr("disabled", true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_view #view-delete").attr("disabled", true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_view #view-write").attr("disabled", true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_update #update-cancel").attr("disabled",true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_update #update-send").attr("disabled",true); // write 시 불필요한 버튼 비활성화
+		$(".btn_group_update #update-cancel").hide(); // write 시 불필요한 버튼 가리기
+		$(".btn_group_update #update-send").hide(); // write 시 불필요한 버튼 가리기
+		$(".comment-list").html(''); // write 시 불필요한 댓글 파트 가리기
+		createTempContent(); // 임시글 생성후 표시
 // 										CKFinder.setupCKEditor(editor);
 	}
 	if(mode == "list-mode"){
+		$("#write-mode").hide(); // 리스트 표기시 불필요한 write 파트 가리기
+		$("#view-mode").hide();  // 리스트 표기시 불필요한 view 파트 가리기
+		$(".board-list").show(); // 글 리스트 보이기
 		
-		$("#write-mode").hide();
-		$("#view-mode").hide();
-		$(".board-list").show();
+		$(".article-header .writer-info").html(''); // 글 리스트 표기시 불필요한 내용 지우기
+		$(".article-container").html(''); // 글 리스트 표기시 불필요한 내용 지우기
 		
-		$(".article-header .writer-info").html('');
-		$(".article-container").html('');
-		
-		$(".btn_group_write #write-list").attr("disabled",true);
-		$(".btn_group_write #write-send").attr("disabled",true);
-		$(".btn_group_view #view-list").attr("disabled", true);
-		$(".btn_group_view #view-update").attr("disabled", true);
-		$(".btn_group_view #view-delete").attr("disabled", true);
-		$(".btn_group_view #view-write").attr("disabled", true);
-		$(".btn_group_update #update-cancel").attr("disabled",true);
-		$(".btn_group_update #update-send").attr("disabled",true);
-		$(".btn_group_update #update-cancel").hide();
-		$(".btn_group_update #update-send").hide();
-		$("#write-mode [name='ct_uid']").val('');
-		$(".comment-list").html('');
+		$(".btn_group_write #write-list").attr("disabled",true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_write #write-send").attr("disabled",true); // 글 리스트 표기시 불필요한 버튼 비 활성화 
+		$(".btn_group_view #view-list").attr("disabled", true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_view #view-update").attr("disabled", true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_view #view-delete").attr("disabled", true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_view #view-write").attr("disabled", true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_update #update-cancel").attr("disabled",true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_update #update-send").attr("disabled",true); // 글 리스트 표기시 불필요한 버튼 비 활성화
+		$(".btn_group_update #update-cancel").hide(); // 글 리스트 표기시 불필요한 버튼 가리기
+		$(".btn_group_update #update-send").hide(); // 글 리스트 표기시 불필요한 버튼 가리기
+		$("#write-mode [name='ct_uid']").val(''); // 글 리스트 표기시 글을 선택하지 않았으므로 글을 선택했다는 내용 없애기
+		$(".comment-list").html(''); // 글 리스트 표기시 불필요한 버튼 가리기
 	}
 	
 	if(mode == "view-mode"){
-		$("#write-mode").hide();
-		$(".board-list").hide();
+		$("#write-mode").hide(); // 글작성 부분 가리기
+		$(".board-list").hide(); // 글 목록 부분 가리기
 		
 		
 		
-		$(".btn_group_write #write-list").attr("disabled",true);
-		$(".btn_group_write #write-send").attr("disabled",true);
-		$(".btn_group_view #view-list").attr("disabled", false);
-		$(".btn_group_view #view-write").attr("disabled", false);
-		$(".btn_group_update #update-cancel").attr("disabled",true);
-		$(".btn_group_update #update-send").attr("disabled",true);
-		$(".btn_group_update #update-cancel").hide();
-		$(".btn_group_update #update-send").hide();
+		$(".btn_group_write #write-list").attr("disabled",true); // 글 상세보기시 불필요한 버튼 비 활성화
+		$(".btn_group_write #write-send").attr("disabled",true); // 글 상세보기시 불필요한 버튼 비 활성화
+		$(".btn_group_view #view-list").attr("disabled", false); // 글 상세보기시 필요한 버튼 활성화
+		$(".btn_group_view #view-write").attr("disabled", false); // 글 상세보기시 필요한 버튼 활성화
+		$(".btn_group_update #update-cancel").attr("disabled",true);  // 글 상세보기시 불필요한 버튼 비 활성화
+		$(".btn_group_update #update-send").attr("disabled",true); // 글 상세보기시 불필요한 버튼 비 활성화
+		$(".btn_group_update #update-cancel").hide(); // 글 상세보기시 불필요한 버튼 가리기
+		$(".btn_group_update #update-send").hide(); // 글 상세보기시 불필요한 버튼 가리기
 		
-		if(viewItem.member[0].id == username){
+		if(viewItem.member[0].id == username){ // 상세보기시 해당 글을 보는 유저와 글쓴이가 동일할 때 수정, 삭제 버튼 활성화 및 보이기
 			$(".btn_group_view #view-update").show();
 			$(".btn_group_view #view-delete").show();
 			$(".btn_group_view #view-update").attr("disabled", false);
 			$(".btn_group_view #view-delete").attr("disabled", false);
-		}else{
+		}else{// 상세보기시 해당 글을 보는 유저와 글쓴이가 동일하지 않을때 수정, 삭제 버튼 비활성화 및 보이지 않기
 			$(".btn_group_view #view-update").hide();
 			$(".btn_group_view #view-delete").hide();
 			$(".btn_group_view #view-update").attr("disabled", true);
 			$(".btn_group_view #view-delete").attr("disabled", true);
 		}
 		
-		$("#view-mode").show();
+		$("#view-mode").show(); // view 파트 보이기
 		
-		$("#view-mode .view-title").text(viewItem.data[0].ct_title);
-		var userinfo = 
+		$("#view-mode .view-title").text(viewItem.data[0].ct_title); // 글 상세 페이지 제목 넣기
+		var userinfo =  // 글 상세 페이지 글쓴이 정보, 조회수, 작성일 표기
 			"<img src='"+contextPath+"/"+viewItem.member[0].pimg_url+"'>"+
 			"<div class='profile-area'>" +
 				"<div class='profile-info'>"+
@@ -267,44 +268,44 @@ function togglePage(mode){
 			"</div>"
 			;
 		$(".article-header .writer-info").html(userinfo);
-		$(".article-container").html(viewItem.data[0].ct_content);
-		$("#write-mode [name='ct_uid']").val(viewItem.data[0].ct_uid);
-		loadComments(viewItem.data[0].ct_uid);
+		$(".article-container").html(viewItem.data[0].ct_content); // 내용 넣어주기 
+		$("#write-mode [name='ct_uid']").val(viewItem.data[0].ct_uid); // 글의 id 저장해두기 
+		loadComments(viewItem.data[0].ct_uid); // 해당 글의 댓글이 있다면 불러오기
 	}
 	
-	if(mode == "update-mode"){
-		$(".board-list").hide();
-		$("#view-mode").hide();
-		$("#write-mode").show();
+	if(mode == "update-mode"){ 
+		$(".board-list").hide(); // 글 수정시 글 목록 부분 가리기
+		$("#view-mode").hide(); // 글 수정시 글 상세 페이지 가리기
+		$("#write-mode").show(); // 글 수정시 글 쓰기 보이기
 		
-		$(".article-header .writer-info").html('');
-		$(".article-container").html('');
+		$(".article-header .writer-info").html(''); // 글 상세페이지 일부분 초기화
+		$(".article-container").html(''); // 글 상세페이지 일부분 초기화
 		
-		$(".btn_group_write #write-list").attr("disabled",true);
-		$(".btn_group_write #write-send").attr("disabled",true);
-		$(".btn_group_view #view-list").attr("disabled", true);
-		$(".btn_group_view #view-update").attr("disabled", true);
-		$(".btn_group_view #view-delete").attr("disabled", true);
-		$(".btn_group_view #view-write").attr("disabled", true);
-		$(".btn_group_update #update-cancel").attr("disabled",false);
-		$(".btn_group_update #update-send").attr("disabled",false);
-		$(".btn_group_write #write-list").hide();
-		$(".btn_group_write #write-send").hide();
-		$(".btn_group_update #update-cancel").show();
-		$(".btn_group_update #update-send").show();
+		$(".btn_group_write #write-list").attr("disabled",true); // 글 수정시 불필요한 부분 비 활성화
+		$(".btn_group_write #write-send").attr("disabled",true); // 글 수정시 불필요한 부분 비 활성화
+		$(".btn_group_view #view-list").attr("disabled", true); // 글 수정시 불필요한 부분 비 활성화
+		$(".btn_group_view #view-update").attr("disabled", true); // 글 수정시 불필요한 부분 비 활성화
+		$(".btn_group_view #view-delete").attr("disabled", true); // 글 수정시 불필요한 부분 비 활성화
+		$(".btn_group_view #view-write").attr("disabled", true); // 글 수정시 불필요한 부분 비 활성화 
+		$(".btn_group_update #update-cancel").attr("disabled",false); // 글 수정시 필요한 부분 활성화
+		$(".btn_group_update #update-send").attr("disabled",false); // 글 수정시 필요한 부분 활성화
+		$(".btn_group_write #write-list").hide(); // 글 수정시 불필요한 부분 가리기
+		$(".btn_group_write #write-send").hide(); // 글 수정시 불필요한 부분 가리기
+		$(".btn_group_update #update-cancel").show(); // 글 수정시 필요한 부분 보이기
+		$(".btn_group_update #update-send").show(); // 글 수정시 필요한 부분 보이기
 		
-		$("#write-mode #ct_title").val(viewItem.data[0].ct_title);
-		$("#write-mode [name='ct_uid']").val(viewItem.data[0].ct_uid);
+		$("#write-mode #ct_title").val(viewItem.data[0].ct_title); // 글 제목이 있다면 제목 란에 미리 넣어두기
+		$("#write-mode [name='ct_uid']").val(viewItem.data[0].ct_uid); // 해당 글에 대한 uid 저장해두기
 		
-		CKEDITOR.replace('editor1',
+		CKEDITOR.replace('editor1', // 글작성 에디터 설정 
 		{
 			width:'100%',
 			height: '450px',
 			allowedContent: true,
-			uploadUrl: contextPath+'/group/studyboard/file/'+viewItem.data[0].ct_uid+"?responseType=json"
+			uploadUrl: contextPath+'/group/studyboard/file/'+viewItem.data[0].ct_uid+"?responseType=json" // 에디터에 사진을 끌어다 놓을 시 업로드하게 되는 url 작성
 		});
 		
-		CKEDITOR.instances.editor1.setData(viewItem.data[0].ct_content);
+		CKEDITOR.instances.editor1.setData(viewItem.data[0].ct_content); // 사용자에게 textarea가 아닌 editor가 보이므로 editor에 원래 내용 넣어주기
 		
 		
 	}
@@ -327,12 +328,12 @@ function createTempContent(){
 					
 					$("#write-mode [name='ct_uid']").val(data.data[0].ct_uid);
 					
-					CKEDITOR.replace('editor1',
+					CKEDITOR.replace('editor1', // 글작성 에디터 설정
 					{
 						width:'100%',
 						height: '450px',
 						allowedContent: true,
-						uploadUrl: contextPath+'/group/studyboard/file/'+$("#write-mode [name='ct_uid']").val()+"?responseType=json"
+						uploadUrl: contextPath+'/group/studyboard/file/'+$("#write-mode [name='ct_uid']").val()+"?responseType=json" // 에디터에 사진을 끌어다 놓을 시 업로드하게 되는 url 작성
 					
 					});
 					CKEDITOR.instances.editor1.setData("");
@@ -351,7 +352,7 @@ function createTempContent(){
 
 //AJAX 로 폼의 데이터를 전송할 때 CKEDITOR로 변환 된 textarea값을 다시 변경해줘야 데이터가 전달된다.
 
-function CKupdate(){
+function CKupdate(){ // editor에 글이 써있어도 현제 에디터 인스턴스에는 값이 안들어 있다. 값을 넣으려면 업데이트
 
     for ( instance in CKEDITOR.instances )
         CKEDITOR.instances[instance].updateElement();
@@ -380,6 +381,7 @@ function addViewEvent(){
 		});
 	});
 }
+// 상세페이지 보기
 function view(ct){
 	$.ajax({
 			url: contextPath+"/group/studyboard/"+sg_id+"/detail/" + ct,
@@ -392,7 +394,7 @@ function view(ct){
 						window.viewItem = data; //전역변수로 데이터 전달
 						togglePage("view-mode"); // 글 읽기 페이지로 변경
 						
-						$(".board-list table [data-viewcnt='"+viewItem.data[0].ct_uid+"']").text(viewItem.data[0].ct_viewcnt);
+						$(".board-list table [data-viewcnt='"+viewItem.data[0].ct_uid+"']").text(viewItem.data[0].ct_viewcnt); // 보고 있는 글의 조회수 업데이트
 					}
 				}
 			}
@@ -442,7 +444,7 @@ function chkUpdate(){
                     alert("update 성공 " + data.cnt + "개:" + data.status);
 					$("#frmWrite #ct_title").val("");
 					$("#frmWrite #editor1").val("");
-					CKEDITOR.instances.editor1.setData("");
+					CKEDITOR.instances.editor1.setData(""); // 게시글 작성 완료후 에디터에 남아 있는 값 날리기
                     view(ct);
                 } else {
                     alert("INSERT 실패 " + data.status + " : " + data.message);
@@ -453,7 +455,7 @@ function chkUpdate(){
     });  // end $ajax() 
 }
 
-function sendComment(){
+function sendComment(){ // 댓글 작성
 	var data = {
 		'ct_uid':viewItem.data[0].ct_uid, 
 		'id' : username,
@@ -480,7 +482,7 @@ function sendComment(){
     });  // end $ajax() 
 }
 
-
+// 댓글 데이터 불러오기
 function loadComments(ct_uid){
 	$.ajax({
         url : contextPath+"/group/studyboard/comments/"+ct_uid,
@@ -498,6 +500,7 @@ function loadComments(ct_uid){
 
     });  // end $ajax() 	
 }
+// 댓글 데이터 뿌리기
 function showComments(jsonObj){
 	var result = "";
 	var members = {};
@@ -517,13 +520,13 @@ function showComments(jsonObj){
 		result += "<div class='comment-body-wrapper'>"; 
 		result += "<div class='comment-content'>"+jsonObj.data[i].reply+"</div>";	
 		result += "<div class='comment-time'>" + jsonObj.data[i].regdate+"</div>";
-		if(username == jsonObj.data[i].id){
+		if(username == jsonObj.data[i].id){ // 댓글 쓴 사람이 지금의 유저면 수정/삭제 버튼 표시
 			result += "<a id='"+jsonObj.data[i].cm_uid+"fix' class='comment-fix-btn'>수정</a>";
 			result += "<a id='"+jsonObj.data[i].cm_uid+"del' class='comment-del-btn'>삭제</a>";
 			result += "</div>";
 			result += "<div class='edit-dialog'></div>";
 			result += "</li>";
-		}else{
+		}else{ // 아니라면 보이지 않기
 			result += "</div>"
 			result += "</li>";	
 		}
@@ -532,22 +535,22 @@ function showComments(jsonObj){
 	$("ul.comment-list").html(result);
 	
 	$(".comment-info span").text(jsonObj.cnt);
-	$(".comment a.comment-del-btn").click(function(event){
+	$(".comment a.comment-del-btn").click(function(event){ // 댓글 삭제 버튼 클릭시 
 		event.preventDefault();
 		var v = $(this).attr('id').split('del');
 		v = v[0];
-		delCommentByUser(v);
+		delCommentByUser(v); // 댓글 삭제하기
 	});
 	
-	$(".comment a.comment-fix-btn").click(function(event){
+	$(".comment a.comment-fix-btn").click(function(event){ // 댓글 수정 버튼 클릭시 
 		event.preventDefault();
 		var v = $(this).attr('id').split('fix');
 		v = v[0];
-		toggleCommentDialog(v);
+		toggleCommentDialog(v); // 댓글 수정 모드
 	});
 }
 
-function toggleCommentDialog(v){
+function toggleCommentDialog(v){ // 유튜브 댓글 수정 파트 보고 감명 받아 만듬 .. 일반 적으로 보이는 댓글 부분과 그 부분에 수정을 위한 부분을 따로 남겨놓아서 toggle 되게 끔 
 	$("li[id='"+v+"comment"+"'] .comment-body-wrapper").hide();
 	$("li[id='"+v+"comment"+"'] .edit-dialog").show();
 	var text = $("li[id='"+v+"comment"+"'] .comment-body-wrapper .comment-content").text();
@@ -562,10 +565,11 @@ function toggleCommentDialog(v){
 	$("li[id='"+v+"comment"+"'] .edit-dialog #comment-register").click(function(){
 		fixCommentByUser(v);
 	});
-	$("li[id='"+v+"comment"+"'] .edit-dialog #comment-cancel").click(function(){
+	$("li[id='"+v+"comment"+"'] .edit-dialog #comment-cancel").click(function(){ // 댓글 수정 취소시 댓글 다시 리로드
 		loadComments(viewItem.data[0].ct_uid);
 	});
 }
+// 댓글 창에 있는 내용을 가져다가 수정 요청 보냄
 function fixCommentByUser(v){
 	var text = $("li[id='"+v+"comment"+"'] .comment_content").val();
 	alert("text: " + text);
@@ -590,6 +594,7 @@ function fixCommentByUser(v){
 	
 }
 
+// 댓글 삭제
 function delCommentByUser(v){
 	var data = {'cm_uid':v, 'id': username};
 	$.ajax({
